274bdaf13252facbf5659d3f2b2a77bd33038945
lisab1993 <brown.lisa999@gmail.com>
2021-04-06 21:56:53 -0700

    Done with Alfred.

--- main.js
+++ main.js
@@ -1,30 +1,64 @@
 const _ = {}
 
 _.each = (array, iteratee) => {
-  // your code here
+  for (let i = 0; i < array.length; i++) {
+    iteratee(array[i], i, array)
+  }
 }
 
 _.map = (array, iteratee) => {
-  // your code here
+  let output = []
+  for (let i = 0; i < array.length; i++) {
+    output.push(iteratee(array[i], i, array))
+  }
+  return output
 }
 
 _.filter = (array, predicate) => {
-  // your code here
+  let output = []
+  for (let i = 0; i < array.length; i++) {
+    if (predicate(array[i], i, array) == true) {
+      output.push(array[i])
+    }
+  }
+  return output
 }
-
 
 _.find = (array, predicate) => {
-  // your code here
+  for (let i = 0; i < array.length; i++) {
+    if (predicate(array[i], i, array)) {
+      return array[i]
+    } 
+    } 
 }
 
-
+//Inclusive means that you "include" the end values.
+//able to use math.random, per Evan!
 _.random = (min, max) => {
-  // your code here
+  let output = Math.random(max)
+  if (output > min){
+    output = Math.random(max)
+  }
+  return output
 }
+
 
 _.range = (start, stop, step) => {
-  // your code here
+  let output = []
+  let mystep = step
+  if (mystep == undefined){
+    mystep = 1
+  }
+  let x = start
+  while (x < stop){
+    output.push(x)
+    x += mystep
+  }
+  return output
 }
+
+
+_.extend(destination, *sources)
 
 
 export default _
